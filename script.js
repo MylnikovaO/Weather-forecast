cff3b61caadf576e861fa0cc9be41f5bb43a6088
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2021-08-08 17:08:06 +0300

    commit1

---
+++ script.js
@@ -0,0 +1,23 @@
+function SelectDefault() {
+    document.getElementById("select").options[0].selected = true;
+    f1();
+}
+SelectDefault();
+
+function f1() {
+    let val = document.querySelector('.container__inner__select').value;
+    fetch('http://api.openweathermap.org/data/2.5/weather?id=' + `${val}` + '&appid=667d5500475a36c5dde9689bfc79df2e')
+        .then(function (resp) { return resp.json() })
+        .then(function (data) {
+            console.log(data)
+            document.querySelector('.container__inner__box__temp').innerHTML = `${'Температура воздуха'} ${Math.round(data.main.temp - 273)}${'&deg'}`;
+            document.querySelector('.container__inner__box__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+            document.querySelector('.container__inner__box__clouds').innerHTML = `${'Облачность'} ${data.clouds.all}${'%'}`;
+            document.querySelector('.container__inner__box__wind').innerHTML = `${'Скорость ветра'} ${data.wind.speed}${'м/с'}`;
+        })
+        .catch(function () {
+
+        });
+}
+
+document.querySelector('.container__inner__select').onchange = f1;
